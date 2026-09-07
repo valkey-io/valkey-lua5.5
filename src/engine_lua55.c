43c31f00275a036600140f8400b2c7a0ce247db3
secwall <secwall@yandex-team.ru>
2026-09-07 13:11:34 +0200

    Fix forkless support
    Signed-off-by: secwall <secwall@yandex-team.ru>

--- src/engine_lua55.c
+++ src/engine_lua55.c
@@ -823,7 +823,8 @@ VKM_PUBLIC int ValkeyModule_OnLoad(ValkeyModuleCtx *ctx,
 
   ValkeyModule_SetModuleOptions(
       ctx, VALKEYMODULE_OPTIONS_HANDLE_REPL_ASYNC_LOAD |
-               VALKEYMODULE_OPTIONS_HANDLE_ATOMIC_SLOT_MIGRATION);
+           VALKEYMODULE_OPTIONS_HANDLE_ATOMIC_SLOT_MIGRATION |
+           VALKEYMODULE_OPTIONS_HANDLE_FORKLESS);
 
   if (ValkeyModule_RegisterStringConfig(ctx, "engine-name", DEFAULT_ENGINE_NAME,
                                         VALKEYMODULE_CONFIG_IMMUTABLE,
